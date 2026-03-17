703d409d28ecc18dec6cfdcd4c030db900d37e46
Julius Hinze <julius.hinze@grafana.com>
2026-03-17 17:04:59 +0100

    address review findings

--- pkg/distributor/distributor.go
+++ pkg/distributor/distributor.go
@@ -1908,7 +1908,7 @@ func (d *Distributor) prePushMaxSeriesLimitMiddleware(next PushFunc) PushFunc {
 
 		if len(req.Timeseries) == 0 {
 			// All series have been rejected, no need to talk to ingesters.
-			return newActiveSeriesLimitedError(totalTimeseries, len(rejectedHashes), d.limits.MaxActiveOrGlobalSeriesPerUser(tenantID))
+			return newActiveSeriesLimitedError(totalTimeseries, len(rejectedHashes), d.limits.MaxActiveOrGlobalSeriesPerUser(fullTenantID))
 		}
 
 		// If there's an error coming from the ingesters, prioritize that one.
@@ -1917,7 +1917,7 @@ func (d *Distributor) prePushMaxSeriesLimitMiddleware(next PushFunc) PushFunc {
 		}
 
 		if len(rejectedHashes) > 0 {
-			return newActiveSeriesLimitedError(totalTimeseries, len(rejectedHashes), d.limits.MaxActiveOrGlobalSeriesPerUser(tenantID))
+			return newActiveSeriesLimitedError(totalTimeseries, len(rejectedHashes), d.limits.MaxActiveOrGlobalSeriesPerUser(fullTenantID))
 		}
 
 		return nil
